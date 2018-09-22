1a0460faa644aafdfcb20a56c1ba8411b7cab46d
Washington Candeia <araujowash@gmail.com>
2018-09-22 19:33:43 -0300

    Deletar

--- fgsea/gseaGBSxZIKV.R
+++ fgsea/gseaGBSxZIKV.R
@@ -17,18 +17,23 @@ library(DT)
 res <- read_csv('tab/contr_GBS_vs_zika_GSEA.csv')
 
 # Anotações dos símbolos a partir do Ensembl.
+# Cria um data frame (ens2symbol) contendo colunas SYMBOL e ENSEMBL.
 ens2symbol <- AnnotationDbi::select(org.Hs.eg.db,
                                     key=res$ensembl_id, 
                                     columns="SYMBOL",
                                     keytype="ENSEMBL")
 
-# Criar a coluna de símbolos a partir dos IDs Ensembl, da primeira coluna.
+# Criar a coluna de símbolos a partir dos IDs Ensembl da primeira coluna.
+# as_tibble: Coerce lists and matrices to data frames.
+# https://tibble.tidyverse.org/
+# Tibble é uma forma modernizada de data frames.
 ens2symbol <- as_tibble(ens2symbol)
 
 # Confirmar:
 head(ens2symbol, 10)
 
-# Unir a coluna SYMBOL ao data frame:
+# Unir a coluna SYMBOL ao data frame a ser analisado (res).
+# inner_join: Funçao de dplyr usada para juntar tiblle 'ens2symbol' com 'res'.
 res <- inner_join(res, ens2symbol, by = c('ensembl_id'='ENSEMBL'))
 
 # Confirmar:
@@ -65,6 +70,7 @@ fgseaRes <- fgsea(pathways=pathways.GObp, stats=ranks, nperm=10000) #maxSize=500
 
 # Funcionou
 head(fgseaRes, 3)
+
 
 # Estatisticas:
 ## Fazendo contraste entre Up e Down
@@ -110,3 +116,42 @@ plotEnrichment(pathways.GObp[["GO_POSITIVE_REGULATION_OF_VIRAL_TRANSCRIPTION"]],
 # plotGseaTable(examplePathways[mainPathways], exampleRanks, fgseaRes, gseaParam = 0.5)
 
 
+
+##------------------ Se quiser organizar os resultados ------------------
+# Tidy de resultados (apenas para observar em uma tabela DT).
+fgseaResTidy <- fgseaRes %>%
+  as_tibble() %>%
+  arrange(desc(NES))
+
+# Uma tabela mais interessante.
+# Resultará em uma tabela que se utiliza de javascript.
+fgseaResTidy %>% 
+  dplyr::select(-leadingEdge, -ES, -nMoreExtreme) %>% 
+  arrange(padj) %>% DT::datatable()
+  
+## Testes sem relaçao com análise:
+# Exibir colunas pathway com NES e padj:
+fgseaRes[1:10,c("pathway", "NES","padj")]
+fgseaRes[1:10][['pathway']]
+fgseaResTidy[1:10,c("pathway", "NES","padj")]
+fgseaResTidy[fgseaResTidy$pval < 0.01, ]
+fgseaResTidy[fgseaResTidy$pval < 0.01, ]$pathway
+
+# Selecionar por pval < 0.01:
+fgseaResTidy[fgseaResTidy$pval < 0.01, ]$pathway
+head(fgseaRes[order(pval), ], 50)
+
+# Selecionar por padj < 0.05:
+fgseaResTidy[fgseaResTidy$padj < 0.05, ]$pathway
+# Ordenar até 61 por pval:
+fgsea.table <- head(fgseaRes[order(pval), ], 61)
+
+df <- data.frame(matrix(unlist(fgsea.table), byrow=T))
+df <- data.frame(matrix(unlist(fgsea.table), byrow=T),stringsAsFactors=FALSE)
+head(df, 60)
+write.table(df, 'dataframe.txt')
+
+# Tentar usar somente os 60 primeiros:
+fgseaResTidy[fgseaResTidy$pval <= 0.001338688, ]$pathway
+fgseaResTidy[fgseaResTidy$pval, ]$pathway
+##----------------------------------------------------------------
