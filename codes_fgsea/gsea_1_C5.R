61580db363d0d0bef722bf4a0f46cde241512e90
Washington Araujo <araujowash@gmail.com>
2019-11-02 00:39:53 -0300

    Adição C2,C5,C7

---
+++ codes_fgsea/gsea_1_C5.R
@@ -0,0 +1,12 @@
+##---------------------------
+# Usando fgsea em GBS
+# Data: 01/11/2019
+# Washington Candeia
+# GSEA: C5 GO 
+# GBS (recuperados) x ZIKV
+##---------------------------
+library(tidyverse)
+library(fgsea)
+library(org.Hs.eg.db)
+library(ggplot2)
+library(DT)
